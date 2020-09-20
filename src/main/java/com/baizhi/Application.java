c63bed7786bbb2a1e091d8a35bb53f4c3aad66de
gao19980518 <71572078+gao19980518@users.noreply.github.com>
2020-09-20 16:10:56 +0800

    Update Application.java
    远程修改

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -17,6 +17,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+        System.out.println("远程的修改");
     }
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
